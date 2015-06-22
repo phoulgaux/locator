7e7abd27b930610f5f05be0e59d6f541a353d0ef
phreme <choqq@jadamspam.pl>
2015-06-22 16:11:56 +0200

    default url for /loc/ fix

--- rep9django/urls.py
+++ rep9django/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     url(r'^$', 'locator.views.index'),
     url(r'^rest/', include(router.urls)),
     url(r'^api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^loc/$', 'locator.views.index'),
     url(r'^loc/([-@./#&+\w\s]*)$', 'locator.views.index')
 ]
